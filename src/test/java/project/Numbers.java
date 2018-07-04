684b22c408b700e678ece8f4dda3911f72b17540
Evgeniya Rangaeva <janerra32@gmail.com>
2018-07-04 15:35:31 -0400

    commit with .gitignore

---
+++ src/test/java/project/Numbers.java
@@ -0,0 +1,14 @@
+package project;
+
+
+public class Numbers {
+
+	public static void main(String[] args) {
+	
+		int[] nums = {10,23,34,45,56,78,34};
+		
+		for(int i : nums) {
+			System.out.println(i);
+		}
+
+}}
